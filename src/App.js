6ad3a9e6660c24338b18a73c3e119f41bf49b301
Kaiwenzhu <zhuk34@mcmaster.ca>
2022-10-27 02:56:08 -0400

    init app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Resources from "./components/resources"
+import Tables from "./components/tables"
+
+function App() {
+  return (
+    <div className="App">
+      <Tables/>
+      <Resources/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/resources.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function resources() {
+  return (
+    // 外部
+    <div>
+        
+    </div>
+  )
+}
